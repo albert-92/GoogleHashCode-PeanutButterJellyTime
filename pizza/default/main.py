e68ded2f3524d83f2909c3528eaeced401c8cc6e
Tim <tim.heming@tu-dortmund.de>
2017-02-22 22:31:42 +0100

    main stub

--- pizza/default/main.py
+++ pizza/default/main.py
@@ -1,4 +1,5 @@
 from pizza.default.fileInput import readAndParseInputFile
+import math
 
 inputPath = '../input/'
 
@@ -11,3 +12,16 @@ info, pizza = readAndParseInputFile(exampleFile)
 
 print(info)
 print(pizza)
+
+size = math.ceil(math.sqrt(int(info['maxCellsPerSlice'])))
+
+slices = []
+numSlices = 0
+
+for x in range(0, int(info['rows'])-size+1, size):
+    for y in range(0, int(info['columns'])-size+1, size):
+        slices.append([x, x+size, y, y+size])
+        numSlices += 1
+
+print(numSlices)
+print(slices)
