7c04aa70cb7e5182abfcdab11146a96b58ec61ad
Tim <tim.heming@tu-dortmund.de>
2017-02-22 22:51:13 +0100

    main stub

--- pizza/default/main.py
+++ pizza/default/main.py
@@ -10,24 +10,23 @@ smallFile = 'small'
 mediumFile = 'medium'
 bigFile = 'big'
 
-currentFile = smallFile
+currentFile = exampleFile
 
 info, pizza = readAndParseInputFile(inputPath + currentFile + '.in')
 
 print(info)
 print(pizza)
 
-size = math.ceil(math.sqrt(int(info['maxCellsPerSlice'])))
+size = math.floor(math.sqrt(int(info['maxCellsPerSlice'])))
 
 slices = []
-numSlices = 0
 
 for x in range(0, int(info['rows'])-size+1, size):
     for y in range(0, int(info['columns'])-size+1, size):
-        slices.append([x, x+size, y, y+size])
-        numSlices += 1
+        xend = x+size
+        yend = y+size
+        slices.append([x, y, xend, yend])
 
-print(numSlices)
 print(slices)
 
 parseAndSaveOutputFile(slices, outputPath + currentFile + '.out')
